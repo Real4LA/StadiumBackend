e1807c7bcb08e97a3de3770135fb79dfc0e725b6
Real4LA <mohamedalaaelabed@gmail.com>
2025-01-23 00:25:10 +0100

    requirements update

--- stadium_api/apps.py
+++ stadium_api/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StadiumApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stadium_api'
+
+    def ready(self):
+        import stadium_api.signals  # noqa
---
+++ stadium_api/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db import DEFAULT_DB_ALIAS
+
+@receiver(post_migrate)
+def create_superuser(sender, **kwargs):
+    if kwargs.get('using', DEFAULT_DB_ALIAS) != DEFAULT_DB_ALIAS:
+        return
+
+    # Delete all existing users
+    User.objects.all().delete()
+    
+    # Create superuser if it doesn't exist
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@admin.com',
+            password='admin'
+        )
+        print('Superuser created successfully') 
