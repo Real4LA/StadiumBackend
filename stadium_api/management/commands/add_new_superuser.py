86c0fff7346d281d68c788c95125664414357db3
Real4LA <mohamedalaaelabed@gmail.com>
2025-01-23 00:18:09 +0100

    requirements update

--- stadium_api/management/commands/add_new_superuser.py
+++ stadium_api/management/commands/add_new_superuser.py
@@ -2,16 +2,17 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    help = 'Creates an additional superuser'
+    help = 'Deletes all existing superusers and creates a new one with admin/admin credentials'
 
     def handle(self, *args, **options):
-        username = 'stadium_admin'
-        if User.objects.filter(username=username).exists():
-            self.stdout.write('User stadium_admin already exists')
-        else:
-            User.objects.create_superuser(
-                username=username,
-                email='stadium.v0.1.0@gmail.com',
-                password='StadiumAdmin2024!'
-            )
-            self.stdout.write(f'New superuser {username} created successfully')
+        # Delete all existing superusers
+        User.objects.filter(is_superuser=True).delete()
+        self.stdout.write('Deleted all existing superusers')
+
+        # Create new superuser with simple credentials
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@admin.com',
+            password='admin'
+        )
+        self.stdout.write('Created new superuser with username: admin, password: admin')
