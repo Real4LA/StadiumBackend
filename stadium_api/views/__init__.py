18f4f0c029c33cd38cf678e9f4c604ce4e6aa0fb
Real4LA <mohamedalaaelabed@gmail.com>
2025-01-23 02:26:37 +0100

    requirements update

--- stadium_api/views/__init__.py
+++ stadium_api/views/__init__.py
@@ -2,9 +2,13 @@
 
 from .calendar_views import CalendarAPIView
 from .user_views import UserViewSet, user_login
+from .auth import register_user, token_obtain_pair, get_user_info
 
 __all__ = [
     'CalendarAPIView',
     'UserViewSet',
     'user_login',
+    'register_user',
+    'token_obtain_pair',
+    'get_user_info',
 ] 
