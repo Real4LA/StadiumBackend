d9b5353e62eed5378d8930055d73eaf884488699
Real4LA <mohamedalaaelabed@gmail.com>
2025-01-25 14:21:13 +0100

    fixed .env

--- backend/settings.py
+++ backend/settings.py
@@ -221,7 +221,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Tottenham Stadium <noreply@stadium.com>')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Tottenham Stadium <stadium.v0.1.0@gmail.com>').strip('"')
 EMAIL_TIMEOUT = 30
 EMAIL_USE_SSL = False
 
