6b3c9a73bc067fe255c26fe85b68f2a042cb0e4f
Real4LA <mohamedalaaelabed@gmail.com>
2025-01-25 14:12:57 +0100

    fixed .env

--- backend/settings.py
+++ backend/settings.py
@@ -219,9 +219,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'stadium.v0.1.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'nzst ooly swnz hcwx'  # New app password
-DEFAULT_FROM_EMAIL = 'Tottenham Stadium <stadium.v0.1.0@gmail.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Tottenham Stadium <noreply@stadium.com>')
 EMAIL_TIMEOUT = 30
 EMAIL_USE_SSL = False
 
