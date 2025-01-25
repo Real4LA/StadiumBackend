c000327325f7d50437a581691f4b55af543f6055
Real4LA <mohamedalaaelabed@gmail.com>
2025-01-25 14:58:51 +0100

    fixed .env

--- stadium_api/views/__init__.py
+++ stadium_api/views/__init__.py
@@ -1,13 +1,15 @@
 # This file makes the views directory a Python package 
 
 from .user_views import UserViewSet, user_login
-from .auth import register_user
+from .auth import register_user, request_password_reset, reset_password
 from .calendar_views import available_slots, book_slot, cancel_booking, my_bookings
 
 __all__ = [
     'UserViewSet',
     'user_login',
     'register_user',
+    'request_password_reset',
+    'reset_password',
     'available_slots',
     'book_slot',
     'cancel_booking',
